0649e1f027751117e035ff11bd347d9e7d2276df
Trent <tbiggs@sdsu.edu>
2022-05-26 08:52:01 -0700

    Update githubR_interfacing.R

--- githubR_interfacing.R
+++ githubR_interfacing.R
@@ -13,3 +13,11 @@ library(httr)
 
 tbiggsurl = "https://raw.githubusercontent.com/tbiggsgithub/github_howto/main/data/rainfall_chirps_properties_LotIDfin_1981_2019.csv?token=GHSAT0AAAAAABU7LZK46EG7OI4DOYJP45WOYUPT77Q"
 x = read.csv(tbiggsurl)
+dim(x)
+
+# After deleting rows in rainfall, this is the new token:
+tbiggsurl.postupdate2 = "https://raw.githubusercontent.com/tbiggsgithub/github_howto/main/data/rainfall_chirps_properties_LotIDfin_1981_2019.csv?token=GHSAT0AAAAAABU7LZK5ZKX6H2MYO2XREE74YUPUG5A"
+x2 = read.csv(tbiggsurl.postupdate2)
+dim(x2)
+
+#  Seems like the token renews every time.  May be difficult to use private repositories to share data.
