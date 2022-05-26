0210363eebdfe7881eebb8dd5c90030834147895
Trent <tbiggs@sdsu.edu>
2022-05-26 08:46:58 -0700

    Delete rows in datafile
    Deleted rows in rainfall file to test if same url works

--- githubR_interfacing.R
+++ githubR_interfacing.R
@@ -4,21 +4,12 @@ ghub.link = "https://github.com/tbiggsgithub/NSF_Amazon.git"
 
 ghub.link.trial = "https://github.com/tbiggsgithub/github_howto.git"
 
-
 # https://stackoverflow.com/questions/14441729/read-a-csv-from-github-into-r
 library(RCurl)
-#x <- getURL("https://raw.github.com/aronlindberg/latent_growth_classes/master/LGC_data.csv")
-#y <- read.csv(text = x)
+library(curl)
+library(httr)
 
+# In github, find the file, choose 'View Raw", and copy the URL.
 
-#urlfile<-'https://raw.github.com/aronlindberg/latent_growth_classes/master/LGC_data.csv'
-#dsin<-read.csv(urlfile)
-
-
-# Does not work:  cannot open the connection
-tbiggsurl = "https://raw.githubusercontent.com/tbiggsgithub/github_howto/main/data/rainfall_chirps_properties_LotIDfin_1981_2019.csv?token=GHSAT0AAAAAABU7LZK4BDFMNZ4N42SO43DCYUPTPWA"
+tbiggsurl = "https://raw.githubusercontent.com/tbiggsgithub/github_howto/main/data/rainfall_chirps_properties_LotIDfin_1981_2019.csv?token=GHSAT0AAAAAABU7LZK46EG7OI4DOYJP45WOYUPT77Q"
 x = read.csv(tbiggsurl)
-
-# Does not work: file not found
-xurl = getURL(tbiggsurl)
-x = read.csv(text=xurl)
